47a10843cafcfa33f1e7230adb887af826c76ad4
Sathish300997 <123661196+Sathish300997@users.noreply.github.com>
2023-09-06 15:03:35 +0530

    Delete node/First.js

--- node/First.js
+++
@@ -1,9 +0,0 @@
-var fs=require("fs")
-fs.readFile('demo.txt',function(err,data)
-{
-    if(err)
-    {
-        return console.log.error(err);
-    }
-    console.log("async data" + data.toString());
-});
